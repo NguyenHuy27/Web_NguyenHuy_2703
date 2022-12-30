0c4320d2d90c43cf4ea766f575bd7bb32b73b27f
Nguyen Quoc Huy <93436544+NguyenHuy27@users.noreply.github.com>
2022-12-30 11:18:24 +0700

    Add file

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+/*================ MENU SHOW Y HIDDEN ================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW  =====*/ 
+/* Validate if constant exists */ 
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/ 
+/* Validate if constant exists */ 
+
+
+/*================ REMOVE MENU MOBILE ================*/
+
+
+/*================ ACCORDION SKILL ================*/
+
+
+/*================ QUALIFICATION ================*/
+
+
+/*================ SERVVICES MODAL ================*/
